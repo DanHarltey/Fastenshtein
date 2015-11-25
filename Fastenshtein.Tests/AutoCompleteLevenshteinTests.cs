5074bb48035925175848067d9804c1d57d603204
Dan <Dan@FackEmail.com>
2015-11-25 21:40:05 +0000

    adding AutoCompleteLevenshtein

---
+++ Fastenshtein.Tests/AutoCompleteLevenshteinTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Fastenshtein.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AutoCompleteLevenshteinTests
+    {
+        [TestMethod]
+        public void Stops_At_Value1_Lenght_Test()
+        {
+            Test("test", "test ignored", 0);
+        }
+
+        [TestMethod]
+        public void Shorter_Value2_Is_Distance_Test()
+        {
+            Test("test", "tes", 1);
+        }
+
+        [TestMethod]
+        public void Deletion_Adds_Two_Distance_Test()
+        {
+            Test("test", "tst ignored", 2); // deletion
+        }
+
+        [TestMethod]
+        public void Subsitation_Adds_One_Distance_Test()
+        {
+            Test("test", "tast ignored", 1); // subsitation
+        }
+
+        [TestMethod]
+        public void Addation_Adds_Two_Distance_Test()
+        {
+            Test("teest", "test ignored", 2); // addation
+        }
+
+        [TestMethod]
+        public void Transposition_Adds_Two_Distance_Test()
+        {
+            Test("test", "tset ignored", 2); // transposition
+        }
+
+        [TestMethod]
+        public void Different_Case_Adds_One_Distance_Test()
+        {
+            Test("test", "Test", 1); // case 
+        }
+
+        [TestMethod]
+        public void EmtpyString_Returns_Length_Test()
+        {
+            Test("test", string.Empty, 4);
+            Test(string.Empty, "test", 0);
+        }
+
+        [TestMethod]
+        public void EmtpyStrings_Returns_Zero_Test()
+        {
+            Test(string.Empty, string.Empty, 0);
+        }
+
+        private void Test(string value1, string value2, int expected)
+        {
+            int actual = AutoCompleteLevenshtein.Distance(value1, value2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Fastenshtein/AutoCompleteLevenshtein.cs
@@ -0,0 +1,67 @@
+﻿namespace Fastenshtein
+{
+    using Microsoft.SqlServer.Server;
+
+    /// <summary>
+    /// Measures the difference between two strings.
+    /// Uses the Levenshtein string difference algorithm.
+    /// </summary>
+    public static class AutoCompleteLevenshtein
+    {
+        /// <summary>
+        /// Compares the two strings and returns a measure of their summarily with 0 being an exact match.
+        /// </summary>
+        /// <param name="value1">the incomplete value entered by the user</param>
+        /// <param name="value2">the value to compare value1 against</param>
+        /// <returns>0 exact match less a positive value, lower the value the best match</returns>
+        [SqlFunction(
+            Name = "AutoCompleteLevenshteinDistance",
+            DataAccess = DataAccessKind.None,
+            SystemDataAccess = SystemDataAccessKind.None,
+            IsDeterministic = true,
+            IsPrecise = true)]
+        public static int Distance(string value1, string value2)
+        {
+            int[] costs = new int[value1.Length + 1];
+
+            // Add indexing for insertion to first row
+            for (int i = 0; i < costs.Length; costs[i] = i++)
+            {
+            }
+
+            int minSize = value1.Length < value2.Length ? value1.Length : value2.Length;
+
+            for (int i = 0; i < minSize; i++)
+            {
+                costs[0] = i + 1;
+                int addationCost = i;
+
+                for (int j = 0; j < value1.Length; j++)
+                {
+                    int cost;
+                    int insertionCost = costs[j + 1];
+
+                    if (value2[i] == value1[j])
+                    {
+                        cost = addationCost;
+                    }
+                    else
+                    {
+                        cost = insertionCost < addationCost ?
+                            insertionCost :         // insertion
+                            addationCost;           // addation
+
+                        cost = (costs[j] < cost ?
+                            costs[j] :              // deletion
+                            cost) + 1;
+                    }
+
+                    costs[j + 1] = cost;
+                    addationCost = insertionCost;
+                }
+            }
+
+            return costs[value1.Length];
+        }
+    }
+}
