a2aa68552187b7aad3c91247e120262cacc6a50f
Dan <Dan@FakeEmail.com>
2017-01-23 16:34:43 +0000

    updating nuget with .net core

--- src/Fastenshtein/Properties/AssemblyInfo.cs
+++ src/Fastenshtein/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fastenshtein")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The fastest Levenshtein on NuGet")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fastenshtein")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
