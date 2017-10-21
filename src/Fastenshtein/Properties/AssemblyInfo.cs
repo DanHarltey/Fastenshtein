7014ce69d7f6fff3ce884d8fc1a0008b7b0f5e44
Dan Hartley <dan.r.hartley@gmail.com>
2017-10-21 19:36:58 +0100

    moving project file to newer 2017 format no project json

--- src/Fastenshtein/Properties/AssemblyInfo.cs
+++ src/Fastenshtein/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿#if !NETSTANDARD1_0
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -35,3 +36,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.4")]
 [assembly: AssemblyFileVersion("1.0.0.4")]
+#endif
