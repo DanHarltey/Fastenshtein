4980d29eeeae176c8bc0c9ecbc74d86515deff2c
Dan Hartley <dan.r.hartley@gmail.com>
2017-10-21 20:04:58 +0100

    updating the build script to newest msbuild

--- src/NuGet/Package.bat
+++ src/NuGet/Package.bat
@@ -3,11 +3,11 @@ rmdir /s /q "..\Fastenshtein\bin\"
 rmdir /s /q "..\FastenshteinFramework\bin\"
 rmdir /s /q "..\FastenshteinPcl\bin\"
 
-"C:\Program Files (x86)\MSBuild\14.0\bin\amd64\msbuild.exe" ..\Fastenshtein\Fastenshtein.csproj /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" ..\Fastenshtein\Fastenshtein.csproj /p:Configuration=Release
 
-"C:\Program Files (x86)\MSBuild\14.0\bin\amd64\msbuild.exe" ..\FastenshteinFramework\FastenshteinFramework.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.0;TargetFrameworkProfile=Client
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" ..\FastenshteinFramework\FastenshteinFramework.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.0;TargetFrameworkProfile=Client
 
-"C:\Program Files (x86)\MSBuild\14.0\bin\amd64\msbuild.exe" ..\FastenshteinPcl\FastenshteinPcl.csproj /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" ..\FastenshteinPcl\FastenshteinPcl.csproj /p:Configuration=Release
 
 C:\NuGet.exe pack Fastenshtein.dll.nuspec
 @pause
