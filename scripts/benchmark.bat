d787319a893d9fb8f504dd9fe76fabaaf4f85867
Dan Hartley <dan.r.hartley@gmail.com>
2020-12-05 20:14:08 +0000

    Adding an appveyor build yml for benchmark scripts in the scripts folder. Windows, ubuntu. Couldn't get macos has they still have a old preview version of net 5 on there.

--- scripts/benchmark.bat
+++ scripts/benchmark.bat
@@ -1,4 +1,12 @@
-dotnet restore ..\
-dotnet build ..\src\Fastenshtein --configuration Release --framework netstandard1.0 --nologo --no-restore
-dotnet build ..\benchmarks\Fastenshtein.Benchmarking --configuration Release --framework net5.0 --nologo --no-restore
-..\benchmarks\Fastenshtein.Benchmarking\bin\Release\net5.0\Fastenshtein.Benchmarking.exe
+dotnet restore ..\ || goto :error
+dotnet build ..\src\Fastenshtein --configuration Release --framework netstandard1.0 --nologo --no-restore || goto :error
+dotnet build ..\benchmarks\Fastenshtein.Benchmarking --configuration Release --framework net5.0 --nologo --no-restore || goto :error
+dotnet build ..\tests\Fastenshtein.Tests --configuration Release --framework net5.0 --nologo --no-restore || goto :error
+dotnet test ..\tests\Fastenshtein.Tests --configuration Release --framework net5.0 --nologo --no-build --no-restore --verbosity normal || goto :error
+..\benchmarks\Fastenshtein.Benchmarking\bin\Release\net5.0\Fastenshtein.Benchmarking.exe || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -6,4 +6,6 @@ set -e
 dotnet restore ../
 dotnet build ../src/Fastenshtein --configuration Release --framework netstandard1.0 --nologo --no-restore
 dotnet build ../benchmarks/Fastenshtein.Benchmarking --configuration Release --framework net5.0 --nologo --no-restore
+dotnet build ../tests/Fastenshtein.Tests --configuration Release --framework net5.0 --nologo --no-restore
+dotnet test ../tests/Fastenshtein.Tests --configuration Release --framework net5.0 --nologo --no-build --no-restore --verbosity normal
 dotnet ../benchmarks/Fastenshtein.Benchmarking/bin/Release/net5.0/Fastenshtein.Benchmarking.dll
