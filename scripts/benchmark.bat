d5272a22e660c2815c620a1cab1f1dd73098a16f
Dan H <dan.r.hartley@gmail.com>
2024-01-11 06:44:47 +0000

    .Net 8 upgrade

--- scripts/benchmark.bat
+++ scripts/benchmark.bat
@@ -1,10 +1,10 @@
 dotnet --info
 dotnet restore ..\ || goto :error
 dotnet build ..\src\Fastenshtein --configuration Release --framework netstandard1.0 --nologo --no-restore || goto :error
-dotnet build ..\benchmarks\Fastenshtein.Benchmarking --configuration Release --framework net7.0 --nologo --no-restore || goto :error
-dotnet build ..\tests\Fastenshtein.Tests --configuration Release --framework net7.0 --nologo --no-restore || goto :error
-dotnet test ..\tests\Fastenshtein.Tests --configuration Release --framework net7.0 --nologo --no-build --verbosity normal || goto :error
-..\benchmarks\Fastenshtein.Benchmarking\bin\Release\net7.0\Fastenshtein.Benchmarking.exe || goto :error
+dotnet build ..\benchmarks\Fastenshtein.Benchmarking --configuration Release --framework net8.0 --nologo --no-restore || goto :error
+dotnet build ..\tests\Fastenshtein.Tests --configuration Release --framework net8.0 --nologo --no-restore || goto :error
+dotnet test ..\tests\Fastenshtein.Tests --configuration Release --framework net8.0 --nologo --no-build --verbosity normal || goto :error
+..\benchmarks\Fastenshtein.Benchmarking\bin\Release\net8.0\Fastenshtein.Benchmarking.exe || goto :error
 
 goto :EOF
 
--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -5,5 +5,5 @@ set -e
 
 dotnet restore ../
 dotnet build ../ --configuration Release --nologo --no-restore
-dotnet test ../tests/Fastenshtein.Tests --configuration Release --framework net7.0 --nologo --no-build --verbosity normal
-dotnet ../benchmarks/Fastenshtein.Benchmarking/bin/Release/net7.0/Fastenshtein.Benchmarking.dll
+dotnet test ../tests/Fastenshtein.Tests --configuration Release --framework net8.0 --nologo --no-build --verbosity normal
+dotnet ../benchmarks/Fastenshtein.Benchmarking/bin/Release/net8.0/Fastenshtein.Benchmarking.dll
