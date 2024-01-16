734b74ab765ec074e35a04eec9ec8321f99498ed
Dan H <dan.r.hartley@gmail.com>
2024-01-16 22:02:22 +0000

    build

--- scripts/dotnet_build.sh
+++ scripts/dotnet_build.sh
@@ -5,7 +5,11 @@ dotnet restore ../
 dotnet build ../ --configuration Release --no-restore /p:ContinuousIntegrationBuild=true
 
 if [[ $1 = "code_coverage" ]]; then
-  dotnet test ../ --configuration Release --no-build --verbosity normal --collect:"XPlat Code Coverage;Format=lcov"
+  dotnet test ../ --configuration Release --no-build --verbosity normal --framework net8.0 --collect:"XPlat Code Coverage;Format=lcov"
+  find -name "coverage.info" -type f -exec mv {} ../coverage.net8.info \;
+
+  dotnet test ../ --configuration Release --no-build --verbosity normal --framework net48 --collect:"XPlat Code Coverage;Format=lcov"
+  find -name "coverage.info" -type f -exec mv {} ../coverage.net48.info \;
 else
   dotnet test ../ --configuration Release --no-build --verbosity normal
 fi
--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -5,15 +5,14 @@ mkdir ../release
 
 dotnet --info > ../release/dotnet_info.txt
 
-find ../tests/Fastenshtein.Tests/TestResults -name coverage.info -exec cp "{}" ../release  \;
-
 find ../src/Fastenshtein/bin/Release -name "Fastenshtein.*.nupkg" -exec cp "{}" ../release/Fastenshtein.nupkg \;
 find ../src/Fastenshtein/bin/Release -name "Fastenshtein.*.snupkg" -exec cp "{}" ../release/Fastenshtein.snupkg \;
 
 gh release create $1 \
   "../release/Fastenshtein.nupkg" \
   "../release/Fastenshtein.snupkg" \
-  "../release/coverage.info#Code coverage report" \
+  "../coverage.net48.info#Code coverage report net48" \
+  "../coverage.net8.info#Code coverage report net8" \
   "../release/dotnet_info.txt#Built with" \
   --draft \
   --generate-notes
