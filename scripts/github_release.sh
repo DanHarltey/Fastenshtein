9a7cc370ecee1b8779b3d532deb9ae6de832bdfe
Dan H <dan.r.hartley@gmail.com>
2024-01-17 22:54:54 +0000

    fixing errors

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -4,8 +4,8 @@ set -e
 gh release create $1 \
   "../release/Fastenshtein.nupkg" \
   "../release/Fastenshtein.snupkg" \
-  "../coverage.net48.info#Code coverage report net48" \
-  "../coverage.net8.info#Code coverage report net8" \
+  "../release/coverage.net48.info#Code coverage report net48" \
+  "../release/coverage.net8.info#Code coverage report net8" \
   "../release/dotnet_info.txt#Built with" \
   --draft \
   --generate-notes
