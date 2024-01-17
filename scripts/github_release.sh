04f076e0ffc4a88bcf13db1062d340b2b7f54607
Dan H <dan.r.hartley@gmail.com>
2024-01-17 22:45:34 +0000

    release

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-mkdir ../release
-
-dotnet --info > ../release/dotnet_info.txt
-
-find ../src/Fastenshtein/bin/Release -name "Fastenshtein.*.nupkg" -exec cp "{}" ../release/Fastenshtein.nupkg \;
-find ../src/Fastenshtein/bin/Release -name "Fastenshtein.*.snupkg" -exec cp "{}" ../release/Fastenshtein.snupkg \;
-
 gh release create $1 \
   "../release/Fastenshtein.nupkg" \
   "../release/Fastenshtein.snupkg" \
