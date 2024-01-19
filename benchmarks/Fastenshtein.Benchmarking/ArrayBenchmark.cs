b4def46eaa9fd59393bded02c2a1de50d3d39c3b
Dan H <dan.r.hartley@gmail.com>
2024-01-19 22:28:10 +0000

    random mess

--- benchmarks/Fastenshtein.Benchmarking/ArrayBenchmark.cs
+++ benchmarks/Fastenshtein.Benchmarking/ArrayBenchmark.cs
@@ -3,20 +3,21 @@
 namespace Fastenshtein.Benchmarking
 {
     [RankColumn]
-    public abstract class ArrayBenchmark
+    public class ArrayBenchmark
     {
         [Params(4, 25, 20_000)]
         public int N;
 
-        protected int[] array;
+        private int[] _array;
         ////protected nuint[] array2;
 
         [GlobalSetup]
         public void SetUp()
         {
-            this.array = new int[N];
+            this._array = new int[N];
             ////this.array2 = new nuint[N];
         }
+
         ////[Benchmark(Baseline = true)]
         ////public int[] Simple()
         ////{
@@ -31,11 +32,44 @@ namespace Fastenshtein.Benchmarking
         [Benchmark(Baseline = true)]
         public int[] SimpleLocal()
         {
-            var tmp = this.array;
+            var tmp = this._array;
 
             for (int i = 0; i < tmp.Length; i++)
             {
                 tmp[i] = i;
+            }
+
+            return tmp;
+        }
+
+        [Benchmark]
+        public int[] SimpleUnroll()
+        {
+            var tmp = this._array;
+
+            if (tmp.Length > 4)
+            {
+                var i = 4;
+                for (; i < tmp.Length; i += 4)
+                {
+                    tmp[i - 3] = i - 3;
+                    tmp[i - 2] = i - 2;
+                    tmp[i - 1] = i - 1;
+                    tmp[i] = i;
+                }
+
+                i -= 4;
+                for (; i < tmp.Length; i++)
+                {
+                    tmp[i] = i;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    tmp[i] = i;
+                }
             }
 
             return tmp;
@@ -58,7 +92,7 @@ namespace Fastenshtein.Benchmarking
         [Benchmark]
         public int[] SimpleBackward()
         {
-            var tmp = this.array;
+            var tmp = this._array;
 
             for (int i = tmp.Length - 1; i >= 0; i--)
             {
@@ -71,9 +105,9 @@ namespace Fastenshtein.Benchmarking
         [Benchmark]
         public int[] UnrollBackward()
         {
-            var tmp = this.array;
+            var tmp = this._array;
             int i = tmp.Length - 1;
-            
+
             for (; i > 3; i -= 4)
             {
                 tmp[i] = i;
@@ -88,6 +122,66 @@ namespace Fastenshtein.Benchmarking
             }
 
             return tmp;
+        }
+
+        [Benchmark]
+        public int[] PointerSimple()
+        {
+            var tmp = this._array;
+            unsafe
+            {
+                // Must pin object on heap so that it doesn't move while using interior pointers.
+                fixed (int* p = tmp)
+                {
+                    for (int i = 0; i < tmp.Length; i++)
+                    {
+                        p[i] = i;
+                    }
+
+                }
+            }
+
+            return tmp;
+        }
+
+        [Benchmark]
+        public int[] PointerUnroll()
+        {
+            var tmp = this._array;
+            unsafe
+            {
+                // Must pin object on heap so that it doesn't move while using interior pointers.
+                fixed (int* p = tmp)
+                {
+                    if (tmp.Length > 4)
+                    {
+                        var i = 4;
+                        for (; i < tmp.Length; i += 4)
+                        {
+                            p[i - 3] = i - 3;
+                            p[i - 2] = i - 2;
+                            p[i - 1] = i - 1;
+                            p[i] = i;
+
+                        }
+
+                        i -= 4;
+                        for (; i < tmp.Length; i++)
+                        {
+                            p[i] = i;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < tmp.Length; i++)
+                        {
+                            p[i] = i;
+                        }
+                    }
+
+                    return tmp;
+                }
+            }
         }
     }
 }
---
+++ tests/Fastenshtein.Tests/ArrayFillTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Fastenshtein.Tests
+{
+    using Fastenshtein.Benchmarking;
+    using System;
+    using Xunit;
+
+    public class ArrayFillTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(1024)]
+        public void Repeated_Distance_Calls_Return_Correct_Distances(int length)
+        {
+            var onTest = new ArrayBenchmark();
+            onTest.N = length;
+            onTest.SetUp();
+
+            var methods = new []
+            { 
+                onTest.SimpleLocal,
+                onTest.SimpleUnroll,
+                onTest.SimpleBackward,
+                onTest.UnrollBackward,
+                onTest.PointerSimple,
+                onTest.PointerUnroll
+            };
+
+            foreach (var method in methods) 
+            {
+                var array = method();
+
+                for (var i = 0; i < array.Length; i++)
+                {
+                    Assert.Equal(i, array[i]); 
+                }
+            }
+        }
+    }
+}
