4a31e9f56633b0a86e76b02efa263600f1445089
Dan H <dan.r.hartley@gmail.com>
2024-01-19 20:38:06 +0000

    fill

---
+++ benchmarks/Fastenshtein.Benchmarking/ArrayBenchmark.cs
@@ -0,0 +1,93 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Fastenshtein.Benchmarking
+{
+    [RankColumn]
+    public abstract class ArrayBenchmark
+    {
+        [Params(4, 25, 20_000)]
+        public int N;
+
+        protected int[] array;
+        ////protected nuint[] array2;
+
+        [GlobalSetup]
+        public void SetUp()
+        {
+            this.array = new int[N];
+            ////this.array2 = new nuint[N];
+        }
+        ////[Benchmark(Baseline = true)]
+        ////public int[] Simple()
+        ////{
+        ////    for (int i = 0; i < array.Length; i++)
+        ////    {
+        ////        this.array[i] = i;
+        ////    }
+
+        ////    return this.array;
+        ////}
+
+        [Benchmark(Baseline = true)]
+        public int[] SimpleLocal()
+        {
+            var tmp = this.array;
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                tmp[i] = i;
+            }
+
+            return tmp;
+        }
+
+        //[Benchmark]
+        //public nuint[] SimpleNuintLocal()
+        //{
+        //    var tmp = this.array2;
+        //    var len = (nuint) tmp.Length;
+
+        //    for (nuint i = 0; i < len; i++)
+        //    {
+        //        tmp[i] = i;
+        //    }
+
+        //    return tmp;
+        //}
+
+        [Benchmark]
+        public int[] SimpleBackward()
+        {
+            var tmp = this.array;
+
+            for (int i = tmp.Length - 1; i >= 0; i--)
+            {
+                tmp[i] = i;
+            }
+
+            return tmp;
+        }
+
+        [Benchmark]
+        public int[] UnrollBackward()
+        {
+            var tmp = this.array;
+            int i = tmp.Length - 1;
+            
+            for (; i > 3; i -= 4)
+            {
+                tmp[i] = i;
+                tmp[i - 1] = i - 1;
+                tmp[i - 2] = i - 2;
+                tmp[i - 3] = i - 3;
+            }
+
+            for (; i >= 0; i--)
+            {
+                tmp[i] = i;
+            }
+
+            return tmp;
+        }
+    }
+}
--- benchmarks/Fastenshtein.Benchmarking/Program.cs
+++ benchmarks/Fastenshtein.Benchmarking/Program.cs
@@ -16,26 +16,28 @@
         {
             DateTime startTime = DateTime.UtcNow;
 
-            if (args.Length != 0 && string.Equals(args[0], "d", StringComparison.OrdinalIgnoreCase))
-            {
-                _ = BenchmarkRunner.Run<FastenshteinDisassembly>();
-            }
-            else if (args.Length != 0 && string.Equals(args[0], "c", StringComparison.OrdinalIgnoreCase))
-            { 
-                _ = BenchmarkRunner.Run<CompetitiveBenchmarkSmallWordsSingleThread>();
-                _ = BenchmarkRunner.Run<CompetitiveBenchmarkNormalWordsSingleThread>();
-                _ = BenchmarkRunner.Run<CompetitiveBenchmarkLargeWordsSingleThread>();
+            ////if (args.Length != 0 && string.Equals(args[0], "d", StringComparison.OrdinalIgnoreCase))
+            ////{
+            ////    _ = BenchmarkRunner.Run<FastenshteinDisassembly>();
+            ////}
+            ////else if (args.Length != 0 && string.Equals(args[0], "c", StringComparison.OrdinalIgnoreCase))
+            ////{ 
+            ////    _ = BenchmarkRunner.Run<CompetitiveBenchmarkSmallWordsSingleThread>();
+            ////    _ = BenchmarkRunner.Run<CompetitiveBenchmarkNormalWordsSingleThread>();
+            ////    _ = BenchmarkRunner.Run<CompetitiveBenchmarkLargeWordsSingleThread>();
 
-                _ = BenchmarkRunner.Run<CompetitiveBenchmarkSmallWordsMultiThread>();
-                _ = BenchmarkRunner.Run<CompetitiveBenchmarkNormalWordsMultiThread>();
-                _ = BenchmarkRunner.Run<CompetitiveBenchmarkLargeWordsMultiThread>();
-            }
-            else
-            {
-                _ = BenchmarkRunner.Run<BenchmarkSmallWordsSingleThread>();
-                _ = BenchmarkRunner.Run<BenchmarkNormalWordsSingleThread>();
-                _ = BenchmarkRunner.Run<BenchmarkLargeWordsSingleThread>();
-            }
+            ////    _ = BenchmarkRunner.Run<CompetitiveBenchmarkSmallWordsMultiThread>();
+            ////    _ = BenchmarkRunner.Run<CompetitiveBenchmarkNormalWordsMultiThread>();
+            ////    _ = BenchmarkRunner.Run<CompetitiveBenchmarkLargeWordsMultiThread>();
+            ////}
+            ////else
+            ////{
+            ////    _ = BenchmarkRunner.Run<BenchmarkSmallWordsSingleThread>();
+            ////    _ = BenchmarkRunner.Run<BenchmarkNormalWordsSingleThread>();
+            ////    _ = BenchmarkRunner.Run<BenchmarkLargeWordsSingleThread>();
+            ////}
+
+            _ = BenchmarkRunner.Run<ArrayBenchmark>();
 
             Console.WriteLine("Completed in : " + (DateTime.UtcNow - startTime));
         }
