b0def67e793bf28367e605a0ade76c8b3325febe
Dan H <dan.r.hartley@gmail.com>
2024-01-22 08:00:16 +0000

    see magic

--- benchmarks/Fastenshtein.Benchmarking/ArrayBenchmark.cs
+++ benchmarks/Fastenshtein.Benchmarking/ArrayBenchmark.cs
@@ -1,4 +1,10 @@
 ﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace Fastenshtein.Benchmarking
 {
@@ -18,16 +24,16 @@ namespace Fastenshtein.Benchmarking
             ////this.array2 = new nuint[N];
         }
 
-        ////[Benchmark(Baseline = true)]
-        ////public int[] Simple()
-        ////{
-        ////    for (int i = 0; i < array.Length; i++)
-        ////    {
-        ////        this.array[i] = i;
-        ////    }
+        [Benchmark()]
+        public int[] Simple()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = i;
+            }
 
-        ////    return this.array;
-        ////}
+            return _array;
+        }
 
         [Benchmark(Baseline = true)]
         public int[] SimpleLocal()
@@ -35,6 +41,24 @@ namespace Fastenshtein.Benchmarking
             var tmp = this._array;
 
             for (int i = 0; i < tmp.Length; i++)
+            {
+                tmp[i] = i;
+            }
+
+            return tmp;
+        }
+
+        [Benchmark()]
+        public int[] SimpleLocalWithCopy()
+        {
+            var tmp = this._array;
+
+            var min = Math.Min(indexes.Length, tmp.Length);
+
+            Buffer.BlockCopy(indexes, 0, tmp, 0, min * sizeof(int));
+
+
+            for (int i = min; i < tmp.Length; i++)
             {
                 tmp[i] = i;
             }
@@ -130,14 +154,15 @@ namespace Fastenshtein.Benchmarking
             var tmp = this._array;
             unsafe
             {
-                // Must pin object on heap so that it doesn't move while using interior pointers.
-                fixed (int* p = tmp)
+                fixed (int* fp = tmp)
                 {
+                    int* p = fp;
+
                     for (int i = 0; i < tmp.Length; i++)
                     {
-                        p[i] = i;
+                        *p = i;
+                        p++;
                     }
-
                 }
             }
 
@@ -150,38 +175,156 @@ namespace Fastenshtein.Benchmarking
             var tmp = this._array;
             unsafe
             {
-                // Must pin object on heap so that it doesn't move while using interior pointers.
-                fixed (int* p = tmp)
+                fixed (int* fp = tmp)
                 {
-                    if (tmp.Length > 4)
-                    {
-                        var i = 4;
-                        for (; i < tmp.Length; i += 4)
-                        {
-                            p[i - 3] = i - 3;
-                            p[i - 2] = i - 2;
-                            p[i - 1] = i - 1;
-                            p[i] = i;
 
-                        }
+                    var length = tmp.Length - 1;
 
-                        i -= 4;
-                        for (; i < tmp.Length; i++)
-                        {
-                            p[i] = i;
-                        }
-                    }
-                    else
+                    int* p = &fp[length];
+
+                    while (length > 2)
                     {
-                        for (int i = 0; i < tmp.Length; i++)
-                        {
-                            p[i] = i;
-                        }
+                        *p = length--;
+                        p--;
+                        *p = length--;
+                        p--;
+                        *p = length--;
+                        p--;
+                        *p = length--;
+                        p--;
                     }
 
-                    return tmp;
+                    while (length > 0)
+                    {
+                        *p = length--;
+                        p--;
+                    }
+                }
+
+                return tmp;
+            }
+        }
+
+        [Benchmark]
+        public int[] UnsafeAdd()
+        {
+            var tmp = this._array;
+
+            ref var reference = ref MemoryMarshal.GetArrayDataReference(tmp);
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                ref var value = ref Unsafe.Add(ref reference, i);
+                value = i;
+            }
+            return tmp;
+        }
+
+        private static readonly int[] indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        private static readonly Vector128<int> indexes128 = Vector128.Create(indexes);
+        private static readonly Vector256<int> indexes256 = Vector256.Create(indexes);
+        private static readonly Vector512<int> indexes512 = Vector512.Create(indexes);
+
+        [Benchmark]
+        public int[] See()
+        {
+            var tmp = this._array;
+            unsafe
+            {
+                fixed (int* fp = tmp)
+                {
+                    int* p = fp;
+
+                    var length = tmp.Length - 1;
+
+                    var addVector = Vector128.Create(Vector128<int>.Count);
+                    var previous = indexes128;
+
+                    while (length >= Vector128<int>.Count)
+                    {
+                        Sse2.Store(p, previous);
+                        p += Vector128<int>.Count;
+
+                        previous = Sse2.Add(previous, addVector);
+                        length -= Vector128<int>.Count;
+                    }
+
+                    for (int i = (tmp.Length - length) - 1; i < tmp.Length; i++)
+                    {
+                        tmp[i] = i;
+                    }
                 }
             }
+
+            return tmp;
+        }
+
+        [Benchmark]
+        public int[] See256()
+        {
+            var tmp = this._array;
+            unsafe
+            {
+                fixed (int* fp = tmp)
+                {
+                    int* p = fp;
+
+                    var length = tmp.Length - 1;
+
+                    var addVector = Vector256.Create(Vector256<int>.Count);
+                    var previous = indexes256;
+
+                    while (length >= Vector256<int>.Count)
+                    {
+                        Avx.Store(p, previous);
+                        p += Vector256<int>.Count;
+
+                        previous = Avx2.Add(previous, addVector);
+                        length -= Vector256<int>.Count;
+                    }
+
+                    for (int i = (tmp.Length - length) - 1; i < tmp.Length; i++)
+                    {
+                        tmp[i] = i;
+                    }
+                }
+            }
+
+            return tmp;
+        }
+
+        [Benchmark]
+        public int[] SeeAny()
+        {
+            var tmp = _array;
+            unsafe
+            {
+                fixed (int* fp = tmp)
+                {
+                    int* p = fp;
+
+                    var length = tmp.Length - 1;
+
+                    var addVector = new Vector<int>(Vector<int>.Count);
+                    var previous = new Vector<int>(indexes);
+
+                    while (length >= Vector<int>.Count)
+                    {
+                        previous.Store(p);
+                        p += Vector<int>.Count;
+
+                        previous += addVector;
+                        length -= Vector<int>.Count;
+                    }
+
+                    for (int i = (tmp.Length - length) - 1; i < tmp.Length; i++)
+                    {
+                        tmp[i] = i;
+                    }
+                }
+            }
+
+            return tmp;
         }
     }
 }
--- tests/Fastenshtein.Tests/ArrayFillTests.cs
+++ tests/Fastenshtein.Tests/ArrayFillTests.cs
@@ -17,20 +17,28 @@
         {
             var onTest = new ArrayBenchmark();
             onTest.N = length;
-            onTest.SetUp();
 
             var methods = new []
-            { 
+            {
+                onTest.Simple,
                 onTest.SimpleLocal,
+                onTest.SimpleLocalWithCopy,
                 onTest.SimpleUnroll,
                 onTest.SimpleBackward,
                 onTest.UnrollBackward,
                 onTest.PointerSimple,
-                onTest.PointerUnroll
+                onTest.PointerUnroll,
+                onTest.UnsafeAdd,
+                onTest.See,
+                onTest.See256,
+                //onTest.See512,
+                onTest.SeeAny,
             };
 
             foreach (var method in methods) 
             {
+                onTest.SetUp();
+
                 var array = method();
 
                 for (var i = 0; i < array.Length; i++)
