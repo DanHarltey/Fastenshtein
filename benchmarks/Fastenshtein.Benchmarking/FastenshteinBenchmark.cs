309ef36606e2eac50a4d24eafb3ee2af4313d0ad
Dan H <dan.r.hartley@gmail.com>
2024-03-23 22:38:43 +0000

    tests

--- benchmarks/Fastenshtein.Benchmarking/FastenshteinBenchmark.cs
+++ benchmarks/Fastenshtein.Benchmarking/FastenshteinBenchmark.cs
@@ -32,6 +32,48 @@ namespace Fastenshtein.Benchmarking
         }
 
         [Benchmark]
+        public void Fastenshtein2()
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                var levenshtein = new global::Fastenshtein.Levenshtein(words[i]);
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    levenshtein.DistanceFrom2(words[j]);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void Fastenshtein3()
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                var levenshtein = new global::Fastenshtein.Levenshtein(words[i]);
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    levenshtein.DistanceFrom3(words[j]);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void Fastenshtein4()
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                var levenshtein = new global::Fastenshtein.Levenshtein(words[i]);
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    levenshtein.DistanceFrom4(words[j]);
+                }
+            }
+        }
+
+        [Benchmark]
         public void FastenshteinStatic()
         {
             for (int i = 0; i < words.Length; i++)
--- src/Fastenshtein/Levenshtein.cs
+++ src/Fastenshtein/Levenshtein.cs
@@ -1,4 +1,9 @@
-﻿namespace Fastenshtein
+﻿#if NET6_0_OR_GREATER
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+#endif
+
+namespace Fastenshtein
 {
     /// <summary>
     /// Measures the difference between two strings.
@@ -29,6 +34,202 @@
         /// </summary>
         public int StoredLength => this.storedValue.Length;
 
+
+#if NET6_0_OR_GREATER
+
+        /// <summary>
+        /// Compares a value to the stored value. 
+        /// Not thread safe.
+        /// </summary>
+        /// <returns>Difference. 0 complete match.</returns>
+        public int DistanceFrom3(string value)
+        {
+            var costs = this.costs;
+            var storedValue = this.storedValue;
+
+            if (costs.Length == 0 || costs.Length != storedValue.Length)
+            {
+                return value.Length;
+            }
+
+            // Add indexing for insertion to first row
+            for (int i = 0; i < costs.Length;)
+            {
+                costs[i] = ++i;
+            }
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                // cost of the first index
+                int cost = i;
+                int previousCost = i;
+
+                // cache value for inner loop to avoid index lookup and bonds checking, profiled this is quicker
+                char value1Char = value[i];
+
+                for (int j = 0; j < storedValue.Length; j++)
+                {
+                    int currentCost = cost;
+
+                    // assigning this here reduces the array reads we do, improvement of the old version
+                    cost = costs[j];// Unsafe.Add(ref refCosts, j);
+
+                    if (value1Char != storedValue[j])
+                    {
+                        if (previousCost < currentCost)
+                        {
+                            currentCost = previousCost;
+                        }
+
+                        if (cost < currentCost)
+                        {
+                            currentCost = cost;
+                        }
+
+                        ++currentCost;
+                    }
+
+                    /* 
+                     * Improvement on the older versions.
+                     * Swapping the variables here results in a performance improvement for modern intel CPU’s, but I have no idea why?
+                     */
+                    costs[j] = currentCost;
+                    previousCost = currentCost;
+                }
+            }
+
+            return costs[costs.Length - 1];
+        }
+
+        /// <summary>
+        /// Compares a value to the stored value. 
+        /// Not thread safe.
+        /// </summary>
+        /// <returns>Difference. 0 complete match.</returns>
+        public int DistanceFrom2(string value)
+        {
+            var costs = this.costs;
+            ref var refCosts = ref MemoryMarshal.GetArrayDataReference(this.costs);
+
+            if (costs.Length == 0)
+            {
+                return value.Length;
+            }
+
+            // Add indexing for insertion to first row
+            for (int i = 0; i < costs.Length;)
+            {
+                costs[i] = ++i;
+            }
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                // cost of the first index
+                int cost = i;
+                int previousCost = i;
+
+                // cache value for inner loop to avoid index lookup and bonds checking, profiled this is quicker
+                char value1Char = value[i];
+
+                for (int j = 0; j < this.storedValue.Length; j++)
+                {
+                    int currentCost = cost;
+
+                    // assigning this here reduces the array reads we do, improvement of the old version
+                    cost = Unsafe.Add(ref refCosts, j);
+
+                    if (value1Char != this.storedValue[j])
+                    {
+                        if (previousCost < currentCost)
+                        {
+                            currentCost = previousCost;
+                        }
+
+                        if (cost < currentCost)
+                        {
+                            currentCost = cost;
+                        }
+
+                        ++currentCost;
+                    }
+
+                    /* 
+                     * Improvement on the older versions.
+                     * Swapping the variables here results in a performance improvement for modern intel CPU’s, but I have no idea why?
+                     */
+                    Unsafe.Add(ref refCosts, j) = currentCost;
+                    previousCost = currentCost;
+                }
+            }
+
+            return costs[costs.Length - 1];
+        }
+
+        /// <summary>
+        /// Compares a value to the stored value. 
+        /// Not thread safe.
+        /// </summary>
+        /// <returns>Difference. 0 complete match.</returns>
+        public int DistanceFrom4(string value)
+        {
+            var costs = this.costs;
+
+            if (costs.Length == 0)
+            {
+                return value.Length;
+            }
+
+            // Add indexing for insertion to first row
+            ref var refCosts = ref MemoryMarshal.GetArrayDataReference(costs);
+            for (int i = 1; i <= costs.Length; i++)
+            {
+                refCosts = i;
+                refCosts = ref Unsafe.Add(ref refCosts, 1);
+            }
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                // cost of the first index
+                int cost = i;
+                int previousCost = i;
+
+                // cache value for inner loop to avoid index lookup and bonds checking, profiled this is quicker
+                char value1Char = value[i];
+
+                for (int j = 0; j < this.storedValue.Length; j++)
+                {
+                    int currentCost = cost;
+
+                    // assigning this here reduces the array reads we do, improvement of the old version
+                    cost = costs[j];
+
+                    if (value1Char != this.storedValue[j])
+                    {
+                        if (previousCost < currentCost)
+                        {
+                            currentCost = previousCost;
+                        }
+
+                        if (cost < currentCost)
+                        {
+                            currentCost = cost;
+                        }
+
+                        ++currentCost;
+                    }
+
+                    /* 
+                     * Improvement on the older versions.
+                     * Swapping the variables here results in a performance improvement for modern intel CPU’s, but I have no idea why?
+                     */
+                    costs[j] = currentCost;
+                    previousCost = currentCost;
+                }
+            }
+
+            return costs[costs.Length - 1];
+        }
+#endif
         /// <summary>
         /// Compares a value to the stored value. 
         /// Not thread safe.
