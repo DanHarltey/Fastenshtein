a3f1f719275b9ac427375f922cc0c51fe76fe789
Dan H <dan.r.hartley@gmail.com>
2024-11-26 22:08:57 +0000

    upgrading to .net 9

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -5,7 +5,7 @@ gh release create $1 \
   "../release/Fastenshtein.nupkg" \
   "../release/Fastenshtein.snupkg" \
   "../release/coverage.net48.info#Code coverage report net48" \
-  "../release/coverage.net8.info#Code coverage report net8" \
+  "../release/coverage.net9.info#Code coverage report net8" \
   "../release/dotnet_info.txt#Built with" \
   --draft \
   --generate-notes
