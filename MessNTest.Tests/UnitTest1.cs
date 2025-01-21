62a4616d4e78666e99f8b5ab14dae39c4a57bdd4
Dan H <dan.r.hartley@gmail.com>
2025-01-21 20:15:53 +0000

    commenting out tests

--- MessNTest.Tests/UnitTest1.cs
+++ MessNTest.Tests/UnitTest1.cs
@@ -1,124 +1,124 @@
-﻿namespace MessNTest.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void SquareGrid()
-        {
-            var xWord = "xxx";
-            var yWord = "yyy";
+﻿////namespace MessNTest.Tests
+////{
+////    public class UnitTest1
+////    {
+////        [Fact]
+////        public void SquareGrid()
+////        {
+////            var xWord = "xxx";
+////            var yWord = "yyy";
 
-            var grid = Program.CalculateGrid(xWord, yWord);
-            var actual = Program.PrintGrid(xWord, yWord, grid);
-
-
-            var expected = @"  xxx
- 0123
-y1123
-y2223
-y3333
-";
-            Assert.Equal(expected, actual);
-        }
+////            var grid = Program.CalculateGrid(xWord, yWord);
+////            var actual = Program.PrintGrid(xWord, yWord, grid);
 
 
-        [Fact]
-        public void SquareDGrid()
-        {
-            var xWord = "xxx";
-            var yWord = "yyy";
-
-            var grid = Program.CalculateGrid(xWord, yWord);
-            var actual = Program.DGrid(xWord, yWord, grid);
-
-            var expected = @"  xxx
- 0123
-y01
-y122
-y2323
-   33
-    3
-";
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void SquareLongGrid()
-        {
-            var xWord = "xxxx";
-            var yWord = "yy";
-
-            var grid = Program.CalculateGrid(xWord, yWord);
-            var actual = Program.PrintGrid(xWord, yWord, grid);
+////            var expected = @"  xxx
+//// 0123
+////y1123
+////y2223
+////y3333
+////";
+////            Assert.Equal(expected, actual);
+////        }
 
 
-            var expected = @"  xxxx
- 01234
-y11234
-y22234
-";
-            Assert.Equal(expected, actual);
-        }
+////        [Fact]
+////        public void SquareDGrid()
+////        {
+////            var xWord = "xxx";
+////            var yWord = "yyy";
 
-        [Fact]
-        public void SquareLongDGrid()
-        {
-            var xWord = "xxxx";
-            var yWord = "yy";
+////            var grid = Program.CalculateGrid(xWord, yWord);
+////            var actual = Program.DGrid(xWord, yWord, grid);
 
-            var grid = Program.CalculateGrid(xWord, yWord);
-            var actual = Program.DGrid(xWord, yWord, grid);
+////            var expected = @"  xxx
+//// 0123
+////y01
+////y122
+////y2323
+////   33
+////    3
+////";
+////            Assert.Equal(expected, actual);
+////        }
+
+////        [Fact]
+////        public void SquareLongGrid()
+////        {
+////            var xWord = "xxxx";
+////            var yWord = "yy";
+
+////            var grid = Program.CalculateGrid(xWord, yWord);
+////            var actual = Program.PrintGrid(xWord, yWord, grid);
 
 
-            var expected = @"  xxxx
- 01234
-y01
-y122
-  32
-  43
-   4
-";
+////            var expected = @"  xxxx
+//// 01234
+////y11234
+////y22234
+////";
+////            Assert.Equal(expected, actual);
+////        }
 
-            Assert.Equal(expected, actual);
-        }
+////        [Fact]
+////        public void SquareLongDGrid()
+////        {
+////            var xWord = "xxxx";
+////            var yWord = "yy";
 
-        [Fact]
-        public void SquarSortGrid()
-        {
-            var xWord = "xx";
-            var yWord = "yyyy";
+////            var grid = Program.CalculateGrid(xWord, yWord);
+////            var actual = Program.DGrid(xWord, yWord, grid);
 
-            var grid = Program.CalculateGrid(xWord, yWord);
-            var actual = Program.PrintGrid(xWord, yWord, grid);
 
-            var expected = @"  xx
- 012
-y112
-y222
-y333
-y444
-";
-            Assert.Equal(expected, actual);
-        }
+////            var expected = @"  xxxx
+//// 01234
+////y01
+////y122
+////  32
+////  43
+////   4
+////";
 
-        [Fact]
-        public void SquarSortDGrid()
-        {
-            var xWord = "xx";
-            var yWord = "yyyy";
+////            Assert.Equal(expected, actual);
+////        }
 
-            var grid = Program.CalculateGrid(xWord, yWord);
-            var actual = Program.DGrid(xWord, yWord, grid);
+////        [Fact]
+////        public void SquarSortGrid()
+////        {
+////            var xWord = "xx";
+////            var yWord = "yyyy";
 
-            var expected = @"  xx
- 012
-y01
-y122
-y2 23
-y3  34
-     4
-";
-            Assert.Equal(expected, actual);
-        }
-    }
-}
+////            var grid = Program.CalculateGrid(xWord, yWord);
+////            var actual = Program.PrintGrid(xWord, yWord, grid);
+
+////            var expected = @"  xx
+//// 012
+////y112
+////y222
+////y333
+////y444
+////";
+////            Assert.Equal(expected, actual);
+////        }
+
+////        [Fact]
+////        public void SquarSortDGrid()
+////        {
+////            var xWord = "xx";
+////            var yWord = "yyyy";
+
+////            var grid = Program.CalculateGrid(xWord, yWord);
+////            var actual = Program.DGrid(xWord, yWord, grid);
+
+////            var expected = @"  xx
+//// 012
+////y01
+////y122
+////y2 23
+////y3  34
+////     4
+////";
+////            Assert.Equal(expected, actual);
+////        }
+////    }
+////}
