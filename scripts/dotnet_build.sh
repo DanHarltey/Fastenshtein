fad8ffbf8439e3cf5de498c6d521dd71ea4f14f0
Dan H <dan.r.hartley@gmail.com>
2025-09-12 21:22:42 +0100

    Fixing issue with test coverage report where it was including the tests in coverage report

--- scripts/dotnet_build.sh
+++ scripts/dotnet_build.sh
@@ -11,10 +11,10 @@ dotnet build ../ --configuration Release --no-restore -p:ContinuousIntegrationBu
 
 if [ $1 = "test_coverage" ] 
 then
-  dotnet test ../ --configuration Release --no-build --verbosity normal --framework net9.0 --collect:"Code Coverage;Format=cobertura"
+  dotnet test ../ --configuration Release --no-build --verbosity normal --framework net9.0 --collect:"Code Coverage;Format=cobertura" --settings:"../.runsettings"
   find ../tests/Fastenshtein.Tests/TestResults/ -name "*.cobertura.xml" -type f -exec mv {} ../release/coverage.net9.xml \;
 
-  dotnet test ../ --configuration Release --no-build --framework net481 --collect:"Code Coverage;Format=cobertura"
+  dotnet test ../ --configuration Release --no-build --framework net481 --collect:"Code Coverage;Format=cobertura" --settings:"../.runsettings"
   find ../tests/Fastenshtein.Tests/TestResults/ -name "*.cobertura.xml" -type f -exec mv {} ../release/coverage.net481.xml \;
 
   xxd -plain -u ../src/Fastenshtein/bin/Release/net462/Fastenshtein.dll | tr -d '\n' > ../release/Fastenshtein_net462.hex
